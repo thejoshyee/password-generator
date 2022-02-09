db11be041061fbdd70726e35312fc9d63cbce446
Josh Yee <joshyee@gmail.com>
2022-02-09 00:00:46 -0800

    finishing touches

--- app.js
+++ app.js
@@ -1,42 +1,27 @@
-//letters: 52
-const letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E",
-"F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-
-//numbers: 10
-const numbers = [0,1,2,3,4,5,6,7,8,9]
-
-//symbols: 29
-const symbols = ["!","@","#","$","%","^","&","*","(",")","-","+","_","=","[","]","{","}","|",":",";","<",">","?","/","~","`",",","."]
-
-
-const ranLettersIndex = Math.floor(Math.random() * letters.length + 1)
-const ranNumIndex = Math.floor(Math.random() * numbers.length + 1)
-const ranSymIndex = Math.floor(Math.random() * symbols.length + 1)
 
 
 function generatePassword() {
-    //generate 10 letters into 4 password variables
-    let password1 = ""
-    let password2 = ""
-    let password3 = ""
-    let password4 = ""
-    // for loop 10 times to push letters / numbers / symbols
-    for (let i = 0; i < 10; i++) {
-        password1 += letters[ranLettersIndex]
-    }
-    for (let i = 0; i < 10; i++) {
-        password1 += numbers[ranNumIndex]
-    }
-    for (let i = 0; i < 10; i++) {
-        password1 += symbols[ranSymIndex]
-    }
+    let result = ''
+    let password1 = document.getElementById("password-1")
+    let password2 = document.getElementById("password-2")
+    let password3 = document.getElementById("password-3")
+    let password4 = document.getElementById("password-4")
 
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*'
+    let charactersLength = characters.length;
+        for ( let i = 0; i < 81; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            password1.textContent = result.slice(0,20)
+            password2.innerHTML = result.slice(20,40)
+            password3.innerHTML = result.slice(40,60)
+            password4.innerHTML = result.slice(60,80)
+           }
     return password1
-
 }
-
-console.log(generatePassword())
-
+        
 
 
-
+function outFunc() {
+    let tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copy to clipboard";
+  }
